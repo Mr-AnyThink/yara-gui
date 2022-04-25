66785fdfcd449fff8c9b9f1f256d8eae38b1be48
Mr.AnyThink <77110956+Mr-AnyThink@users.noreply.github.com>
2022-04-25 11:33:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ pip install yara-python
 
 # Clone yara rules
 # By defalt all rule will be cloned under CDW/rules
-git https://github.com/Yara-Rules/rules.git
+git clone https://github.com/Yara-Rules/rules.git
 
 #update index.yar with absolute full path and remove "MALW_AZORULT.yar" entry as it is causing error for yara-python
 python3 indexWriter.py
